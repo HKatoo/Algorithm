3eb14bffaeb2688a3fb4862b7d7f5fdc16061249
HKatoo <h.kato-7129@keio.jp>
2019-11-20 14:28:43 +0900

    update inverse

---
+++ inverse/binomial-coeff.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+using namespace std;
+#define ALL(x) (x).begin(), (x).end()
+#define OUT(x) cout << (x) << endl
+typedef long long ll;
+
+const int MAX = 1e7;
+const int MOD = 1e9 + 7;
+
+ll fac[MAX], finv[MAX], inv[MAX];
+
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++) {
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+ll COM(int n, int k) {
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+int main() {
+    COMinit();
+    OUT(COM(100000, 50000));
+}
---
+++ inverse/modEuclid.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+using namespace std;
+#define ALL(x) (x).begin(), (x).end()
+#define OUT(x) cout << (x) << endl
+typedef long long ll;
+
+ll modinv(ll a, ll m) {
+    ll b = m, u = 1, v = 0;
+    while (b) {
+        ll t = a / b;
+        a -= t * b;
+        swap(a, b);
+        u -= t * v;
+        swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
+
+int main() { cout << 7 << " = " << modinv(7, 13) << " (mod13)" << endl; }
